37ca4a84579bf1c6d3ff739fb5a26d9bd9834c9b
Andrea Martinelli <andmartinelli@gmail.com>
2017-03-14 23:57:41 +0100

    ReadOnlyStreamBase

---
+++ Shaman.Streams/ReadOnlyStreamBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaman.Runtime
+{
+    public abstract class ReadOnlyStreamBase : Stream
+    {
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        public override void Flush()
+        {
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        private byte[] SingleByteArray = new byte[1];
+        public override int ReadByte()
+        {
+            if (Read(SingleByteArray, 0, 1) == 0) return -1;
+            return SingleByteArray[0];
+        }
+    }
+}
