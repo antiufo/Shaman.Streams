bd841b2206993188b1c435e1ba05e587a5d2cd4b
Andrea Martinelli <andmartinelli@gmail.com>
2017-03-14 23:57:34 +0100

    UnionStream

---
+++ Shaman.Streams/UnionStream.cs
@@ -0,0 +1,53 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaman.Runtime
+{
+    public class UnionStream : ReadOnlyStreamBase
+    {
+        public UnionStream(Stream first, Stream second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        private Stream first;
+        private Stream second;
+
+        public override long Length => first.Length + second.Length;
+
+        public override long Position { get => first.Position + second.Position; set => throw new NotSupportedException(); }
+
+       
+        private bool hasCompletedFirst;
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+
+            if (!hasCompletedFirst)
+            {
+                var r = first.Read(buffer, offset, count);
+                if (r != 0) return r;
+                hasCompletedFirst = true;
+
+            }
+
+            return second.Read(buffer, offset, count);
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                first.Dispose();
+                second.Dispose();
+            }
+        }
+
+    }
+}
+*/
